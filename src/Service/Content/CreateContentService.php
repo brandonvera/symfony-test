6ab6984d74a05bb7c69fe1c63c9c704868c22d41
brandonvera <vera.950b@gmail.com>
2024-07-21 23:59:42 -0400

    validaciones y configuracion de php.ini

--- src/Service/Content/CreateContentService.php
+++ src/Service/Content/CreateContentService.php
@@ -32,17 +32,39 @@ class CreateContentService
     {
         $content = new Content();
 
-        $file = $request->files->get('image');
-
+        $file = $request->files->get('file');
+        
         if ($file instanceof UploadedFile) {
             $mimeType = $file->getMimeType();
-            $fileName = uniqid() . '.' . $file->getClientOriginalExtension();
-            $filePath = $this->containerInterface->getParameter('kernel.project_dir') . '/public/uploads/images/';
+            $fileName = uniqid(). '.'. $file->getClientOriginalExtension();
+            $filePath = $this->containerInterface->getParameter('kernel.project_dir'). '/public/uploads/';
+
+            $allowedTypes = [
+                'image/jpeg',
+                'image/png',
+                'video/mp4',
+            ];
+
+            if (!in_array($mimeType, $allowedTypes)) {
+                throw new \Exception('File Type not allowed');
+            }
+
+            if (strpos($mimeType, 'image/') === 0) {
+                $filePath.= 'images/';
+                $content->setType("Image");
+            } elseif (strpos($mimeType, 'video/') === 0) {
+                $filePath.= 'videos/';
+                $content->setType("Video");
+            }
+
+            if ($file->getSize() > 50 * 1024 * 1024) {
+                throw new \Exception('File size it is too big (max 50MB)');
+            }
+
             $file->move($filePath, $fileName);
             $content->setFileName($fileName);
             $content->setFilePath($filePath);
             $content->setMimeType($mimeType);
-            $content->setType("Image");
         }
 
         $content->setUserId($user);
